07a7b6d06e72a1b5e1fb1cd43a215e232f1f55b2
Yingtong-Tan <ytan3@mtu.edu>
2022-02-09 14:13:32 -0500

    add database setup scripts

---
+++ container/database.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -x
+
+mysql -uroot -pivp -h127.0.0.1 -e "INSERT INTO IVP.user (IVP.user.username, IVP.user.password, IVP.user.accessLevel) VALUES('v2xadmin', 'V2xHub#321', 3)"
+
+set +e
+id plugin >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+	adduser --system --disabled-login --disabled-password --gecos --no-create-home plugin
+fi
+
+usermod -a -G dialout plugin
+
+mkdir -p /var/log/tmx
+chown plugin:adm /var/log/tmx
+chmod 777 /var/log/tmx
+
+mkdir -p /var/www/plugins
+chown plugin:adm /var/www /var/www/plugins
+chmod 755 /var/www/plugins
